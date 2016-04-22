8046826440a63349cb01056e52efbf4ebf7adc3e
Mikael Chudinov <mikael@chudinov.net>
2016-04-22 17:03:55 +0200

    Add Edit and Delete actions.

--- AspMvc5Identity/Controllers/AdminController.cs
+++ AspMvc5Identity/Controllers/AdminController.cs
@@ -63,6 +63,73 @@ namespace AspMvc5Identity.Controllers
             }
         }
 
+        public async Task<ActionResult> Edit(string id)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(string id, string username, string email, string password)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                user.Email = email;
+                IdentityResult validEmail = await UserManager.UserValidator.ValidateAsync(user);
+                if (!validEmail.Succeeded)
+                {
+                    AddErrorsFromResult(validEmail);
+                }
+
+                user.UserName = username;
+                IdentityResult validName = await UserManager.UserValidator.ValidateAsync(user);
+                if (!validName.Succeeded)
+                {
+                    AddErrorsFromResult(validName);
+                }
+
+                IdentityResult validPass = null;
+                if (password != string.Empty)
+                {
+                    validPass = await UserManager.PasswordValidator.ValidateAsync(password);
+                    if (validPass.Succeeded)
+                    {
+                        user.PasswordHash = UserManager.PasswordHasher.HashPassword(password);
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(validPass);
+                    }
+                }
+                
+                if ((validEmail.Succeeded && validName.Succeeded && validPass == null) || (validEmail.Succeeded && validName.Succeeded && password != string.Empty && validPass.Succeeded))
+                {
+                    IdentityResult result = await UserManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(result);
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View(user);
+        }
+
         private void AddErrorsFromResult(IdentityResult result)
         {
             foreach (string error in result.Errors)
