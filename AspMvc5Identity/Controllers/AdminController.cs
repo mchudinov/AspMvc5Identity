0df2800705e62d432bd76b999515e0e407bc28c9
Mikael Chudinov <mikael@chudinov.net>
2016-04-22 15:52:02 +0200

    Add admin users create and index pages.

--- AspMvc5Identity/Controllers/AdminController.cs
+++ AspMvc5Identity/Controllers/AdminController.cs
@@ -1,16 +1,51 @@
-﻿using System.Web;
+﻿using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
+using AspMvc5Identity.Models;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 
 namespace AspMvc5Identity.Controllers
 {
     public class AdminController : Controller
     {
+        private AppUserManager UserManager => HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+
         public ActionResult Index()
         {
             return View(UserManager.Users);
         }
 
-        private AppUserManager UserManager => HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser { UserName = model.Name, Email = model.Email };
+                IdentityResult result = await UserManager.CreateAsync(user,model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
     }
 }
