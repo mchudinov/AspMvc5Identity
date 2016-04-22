344b29f722b8ad95996ae87c265826e114fcda16
Mikael Chudinov <mikael@chudinov.net>
2016-04-22 14:33:29 +0200

    Add AdminController.

---
+++ AspMvc5Identity/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace AspMvc5Identity.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(UserManager.Users);
+        }
+
+        private AppUserManager UserManager => HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+    }
+}
